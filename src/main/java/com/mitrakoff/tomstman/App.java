c0118969c641b8be792d96bc7f1a50d31c5a4d06
mitrakov <artem.mitrakov.ext@letgo.com>
2021-04-14 15:44:05 +0300

    Fix Dirname for symlinks

--- src/main/java/com/mitrakoff/tomstman/App.java
+++ src/main/java/com/mitrakoff/tomstman/App.java
@@ -6,6 +6,7 @@ import com.mitrakoff.tomstman.view.Controller;
 
 public class App {
     public static void main( String[] args ) {
+        // TODO: settings.ini -> ~/.tomstman/settings.ini
         final Model model = new Model();
         final View view = new View();
         final Controller controller = new SimpleController(model);
